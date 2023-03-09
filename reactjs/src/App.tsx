24b653591bb1a3cdf94dbe59f87fc7472edba479
Ruizhi <macryze@icloud.com>
2023-03-09 19:30:29 +0800

    Tables -> Table Layout

--- reactjs/src/App.tsx
+++ reactjs/src/App.tsx
@@ -1,22 +1,34 @@
-export default function App() {
-  return (
-      <table className="border border-separate table-fixed">
-        <thead>
-          <tr>
-            <th className="border">State</th>
-            <th className="border">City</th>
-          </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td className="border">Indiana</td>
-          <td className="border">Indianapolsdflsjflskdjflksjdfljsfdsdfsdfsfsdfsfsdfsdfsfsdsdfsdfsdfsdlksjldfjjis</td>
-        </tr>
-        <tr>
-          <td className="border">sdlfkjsdklfjsdkfjs;ldkfj;lksdjf;lkjas;lfsIndiana</td>
-          <td className="border">Indianapolis</td>
-        </tr>
-        </tbody>
-      </table>
-  )
+// | Class       | Properties           |
+// | ----------- | -------------------- |
+// | table-auto  | table-layout: auto;  |
+// | table-fixed | table-layout: fixed; |
+//
+// Table Layout: Utilities for controlling the table layout algorithm.
+// 1. Auto: Use table-auto to allow the table to automatically size columns to fit the contents of the cell.
+// 2. Fixed: Use table-fixed to allow the table to ignore the content and use fixed widths for columns. The width of the first row will set the column widths for the whole table. You can manually set the widths for some columns and the rest of the available width will be divided evenly amongst the columns without explicit width.
+export default function TableLayout() {
+    return (
+        <table className="table-fixed w-36">
+            <tr>
+                <th className="border">Song</th>
+                <th className="border">Artist</th>
+                <th className="border">Year</th>
+            </tr>
+            <tr>
+                <td className="border">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                <td className="border">Malcolm Lockyer</td>
+                <td className="border">1961</td>
+            </tr>
+            <tr>
+                <td className="border">Witchy Woman</td>
+                <td className="border">The Eagles</td>
+                <td className="border">1972</td>
+            </tr>
+            <tr>
+                <td className="border">Shining Star</td>
+                <td className="border">Earth, Wind, and Fire</td>
+                <td className="border">1975</td>
+            </tr>
+        </table>
+    );
 }
--- reactjs/src/TailwindCSS/Tables/Table Layout.tsx
+++ reactjs/src/TailwindCSS/Tables/Table Layout.tsx
@@ -8,26 +8,28 @@
 // 2. Fixed: Use table-fixed to allow the table to ignore the content and use fixed widths for columns. The width of the first row will set the column widths for the whole table. You can manually set the widths for some columns and the rest of the available width will be divided evenly amongst the columns without explicit width.
 export default function TableLayout() {
   return (
-    <table className="table-auto w-full">
+    <table className="w-36 table-fixed">
       <tr>
-        <th>Song</th>
-        <th>Artist</th>
-        <th>Year</th>
+        <th className="border">Song</th>
+        <th className="border">Artist</th>
+        <th className="border">Year</th>
       </tr>
       <tr>
-        <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-        <td>Malcolm Lockyer</td>
-        <td>1961</td>
+        <td className="border">
+          The Sliding Mr. Bones (Next Stop, Pottersville)
+        </td>
+        <td className="border">Malcolm Lockyer</td>
+        <td className="border">1961</td>
       </tr>
       <tr>
-        <td>Witchy Woman</td>
-        <td>The Eagles</td>
-        <td>1972</td>
+        <td className="border">Witchy Woman</td>
+        <td className="border">The Eagles</td>
+        <td className="border">1972</td>
       </tr>
       <tr>
-        <td>Shining Star</td>
-        <td>Earth, Wind, and Fire</td>
-        <td>1975</td>
+        <td className="border">Shining Star</td>
+        <td className="border">Earth, Wind, and Fire</td>
+        <td className="border">1975</td>
       </tr>
     </table>
   );
